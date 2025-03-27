02b575e373a2c829921d58ffb320205e21359e8f
Dziudita <Stakesheep.team@gmail.com>
2025-03-27 18:54:43 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Leaderboard from '@/components/Leaderboard';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white text-black">
+      <Leaderboard />
+    </main>
+  );
+}
