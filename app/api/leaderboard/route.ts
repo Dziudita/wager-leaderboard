c160bd0b7e1b907fa6af73ea335cca395c49d8ec
Dziudita <Stakesheep.team@gmail.com>
2025-03-27 19:20:55 +0200

    Update route.ts

--- app/api/leaderboard/route.ts
+++ app/api/leaderboard/route.ts
@@ -1,8 +1,8 @@
 export async function GET() {
   const users = [
-    { username: 'User1', total: 1234.56 },
-    { username: 'User2', total: 987.65 },
-    { username: 'User3', total: 654.32 },
+    { username: "User1", total: 1000 },
+    { username: "User2", total: 800 },
+    { username: "User3", total: 500 }
   ];
 
   return Response.json(users);
